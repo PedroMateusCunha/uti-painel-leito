15ad2a0fed4991bd1dd545becf8437e001a8ffba
PedroMateusCunha <29759070+PedroMateusCunha@users.noreply.github.com>
2023-04-10 03:09:39 -0300

    [FIX] Correção das rotas, json e templates

--- painel_leito/main.py
+++ painel_leito/main.py
@@ -8,13 +8,17 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+
+    return templates.TemplateResponse("index.html",
+                                      {"request": request})
+
 @app.get("/sinais", response_class=HTMLResponse)
 async def sinais(request: Request):
-    # Obtém o status da Bomba de Infusão
     response = requests.get("http://10.0.0.254:7001/status")
     data = response.json()["sinais_vitais"]
 
-    # Renderiza o template HTML com os dados do status da Bomba de Infusão
     return templates.TemplateResponse("sinais_vitais.html",
                                       {"request": request,
                                         "temperatura": data["temperatura"],
@@ -26,34 +30,67 @@ async def sinais(request: Request):
 
 @app.get("/bomba", response_class=HTMLResponse)
 async def bomba(request: Request):
-    # Obtém o status da Bomba de Infusão
     response = requests.get("http://10.0.0.254:7002/status")
     data = response.json()["bomba_infusao"]
-
-    # Renderiza o template HTML com os dados do status da Bomba de Infusão
-    return templates.TemplateResponse("bomba_infusao.html",
-                                      {"request": request,
-                                        "taxa_med1": data['taxas']['med1'],
-                                        "taxa_med2": data['taxas']['med2'],
-                                        "taxa_soro": data['taxas']['soro'],
-                                        "qtd_med1": data['quantidades']['med1'],
-                                        "qtd_med2": data['quantidades']['med2'],
-                                        "qtd_soro": data['quantidades']['soro']})
-
-
-@app.get("/cardioversor", response_class=HTMLResponse)
-async def cardioversord(request: Request):
-    # Obtém o status da Bomba de Infusão
-    response = requests.get("http://10.0.0.254:7004/status")
-    data = response.json()
+    
     if data['ligado'] == True:
         ligado = "Ligado"
     else:
         ligado = "Desligado"
-    # Renderiza o template HTML com os dados do status da Bomba de Infusão
+
+    return templates.TemplateResponse("bomba_infusao.html",
+                                      {"request": request,
+                                        "taxa_med1": data["taxas"]["med1"],
+                                        "taxa_med2": data["taxas"]["med2"],
+                                        "taxa_soro": data["taxas"]["soro"],
+                                        "qtd_med1": data["quantidades"]["med1"],
+                                        "qtd_med2": data["quantidades"]["med2"],
+                                        "qtd_soro": data["quantidades"]["soro"],
+                                        "ligado": data["ligado"]})
+
+@app.get("/respirador", response_class=HTMLResponse)
+async def respirador(request: Request):
+    response = requests.get("http://10.0.0.254:7003/status")
+    data = response.json()["respirador"]
+
+    if data['ligado'] == True:
+        ligado = "Ligado"
+    else:
+        ligado = "Desligado"
+
+    return templates.TemplateResponse("respirador.html",
+                                      {"request": request,
+                                        "irpm": data["irpm"],
+                                        "volume_corrente": data["volume_corrente"],
+                                        "ligado": ligado})
+
+@app.get("/cardioversor", response_class=HTMLResponse)
+async def cardioversor(request: Request):
+    response = requests.get("http://10.0.0.254:7004/status")
+    data = response.json()["cardioversor"]
+
+    if data['ligado'] == True:
+        ligado = "Ligado"
+    else:
+        ligado = "Desligado"
+
     return templates.TemplateResponse("cardioversor.html",
                                       {"request": request,
-                                        "marca_passo": data['identificador_marca_passo'],
-                                        "potencia": data['potencia'],
-                                        "frequencia": data['frequencia'],
-                                        "ligado": ligado}) 
+                                        "marca_passo": data["identificador_marca_passo"],
+                                        "potencia": data["potencia"],
+                                        "frequencia": data["frequencia"],
+                                        "ligado": ligado})
+ 
+@app.get("/botao", response_class=HTMLResponse)
+async def botao(request: Request):
+    response = requests.get("http://10.0.0.254:7005/status")
+    data = response.json()["botao_emergencia"]
+
+    if data['ligado'] == True:
+        ligado = "Ligado"
+    else:
+        ligado = "Desligado"
+
+    return templates.TemplateResponse("botao.html",
+                                      {"request": request,
+                                        "ligado": ligado})
